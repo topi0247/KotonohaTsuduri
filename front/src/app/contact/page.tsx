f45d6d0dfc6b736cfd23f2ce37ae83b0cad97110
topi0247 <karubona.ra247@gmail.com>
2024-06-14 01:00:49 +0900

    お問合せフォーム作成

---
+++ front/src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSfPLTVrRmfBdlSEsWiIQi8WlW1Ty6CTrDENtHC9Isz3plE0eA/viewform?embedded=true"
+      width="100%"
+      height="800"
+    >
+      読み込んでいます…
+    </iframe>
+  );
+}
